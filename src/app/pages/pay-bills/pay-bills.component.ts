858d4199935ff7dad5328ad1a217fa26ac1f9d54
AnandAnasane <aanand08a@gmail.com>
2025-06-27 15:31:36 +0530

    update1

---
+++ src/app/pages/pay-bills/pay-bills.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pay-bills',
+  standalone: true,
+  imports: [],
+  templateUrl: './pay-bills.component.html',
+  styleUrl: './pay-bills.component.css'
+})
+export class PayBillsComponent {
+
+}
